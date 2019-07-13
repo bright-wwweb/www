df6f4089128c3d17817812c5024642a3c2cf36b7
eemshi <oreneemshi@gmail.com>
2019-07-12 21:30:17 -0600

    add fonts & 'coming soon' on title hover

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <b className="App-link"> THE BRIGHT WEB </b>
+        <b className="App-link" data-hover="coming soon"> 
+          <span>THE BRIGHT WEB</span>
+        </b>
       </header>
     </div>
   );
